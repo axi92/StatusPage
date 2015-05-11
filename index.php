038d448be912aea08051f19e1d6eb44160b18065
axi92 <phips_92@yahoo.de>
2015-05-11 14:20:25 +0200

    Add Playerlist

--- index.php
+++ index.php
@@ -58,9 +58,79 @@
 ?>
 <link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
 <link href="//bootswatch.com/yeti/bootstrap.min.css" rel="stylesheet" type="text/css" />
+<link href="filter_table.css" rel="stylesheet" type="text/css" />
 
 <script src="//code.jquery.com/jquery.min.js"></script>
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<script src="filter_table.js"></script>
+  
+  
+  <!-- Modal -->
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">Players Online</h4>
+			</div>
+			<div class="modal-body">
+
+			
+				<div class="row">
+					<div class="panel panel-primary filterable">
+						<div class="panel-heading">
+							<h3 class="panel-title">Users</h3>
+							<div class="pull-right">
+								<button class="btn btn-default btn-xs btn-filter"><span class="fa fa-search"></span> Filter</button>
+							</div>
+						</div>
+						<table class="table">
+							<thead>
+								<tr class="filters">
+									<th><input type="text" class="form-control" placeholder="#" disabled></th>
+									<th><input type="text" class="form-control" placeholder="Name" disabled></th>
+									<th><input type="text" class="form-control" placeholder="Playtime" disabled></th>
+									<!-- <th><input type="text" class="form-control" placeholder="Username" disabled></th> -->
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>1</td>
+									<td>Mark</td>
+									<td>Otto</td>
+									<!-- <td>@mdo</td> -->
+								</tr>
+								<tr>
+									<td>2</td>
+									<td>Jacob</td>
+									<td>Thornton</td>
+									<!-- <td>@fat</td> -->
+								</tr>
+								<tr>
+									<td>3</td>
+									<td>Larry</td>
+									<td>the Bird</td>
+									<!-- <td>@twitter</td> -->
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</div>			
+			
+			
+			
+			
+			
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+				<!-- <button type="button" class="btn btn-primary">Save changes</button> -->
+			</div>
+		</div>
+	</div>
+</div>  
+  
+  
   
   
   <div class="container">
@@ -94,7 +164,7 @@
                               </h4>
                               <p class="list-group-item-text">
                                   <span class="label label-<?php echo $arma3_label ?>"><?php echo $arma3_label_text; ?></span>
-								  <span class="badge"><?php echo $players_online; ?> Players Online</span>
+								  <span class="badge" data-toggle="modal" data-target="#myModal"><?php echo $players_online; ?> Players Online</span>
                               </p>
                           </div>
                         

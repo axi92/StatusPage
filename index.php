5fda4a18737cec9ef45402ea27e491aa0350b54e
axi92 <phips_92@yahoo.de>
2015-05-13 09:43:03 +0200

    Google Analytics

--- index.php
+++ index.php
@@ -11,6 +11,16 @@
 	<script src="//code.jquery.com/jquery.min.js"></script>
 	<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 	<script src="filter_table.js"></script>
+	 <script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-25646959-3', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
 	</head>
 <body>
 <?php
@@ -75,18 +85,6 @@
 		$ts3_label_text = 'Not Operational';
 	endif;
 ?>
-<!--
- <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-25646959-3', 'auto');
-  ga('send', 'pageview');
-
-</script>
--->
   
   <!-- Modal -->
 <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">

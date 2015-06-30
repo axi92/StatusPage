b0d48423a8feb64315bb678c8f81f3985334a2b1
axi92 <phips_92@yahoo.de>
2015-06-30 06:55:28 +0200

    Added Lib Array2XML

--- players_xml.php
+++ players_xml.php
@@ -3,6 +3,7 @@
 error_reporting(-1);
 require __DIR__ . '/SourceQuery/SourceQuery.class.php';
 require __DIR__ . '/config.php';
+require __DIR__ . '/array2xml.php';
 $Query = new SourceQuery( );
 
 try
@@ -34,18 +35,18 @@ $Query->Disconnect();
 // creating object of SimpleXMLElement
 $xml_playerlist = new SimpleXMLElement("<?xml version=\"1.0\"?><playerlist></playerlist>");
 
-$playerlist = array( array( Title => "rose", 
-                      Price => 1.25,
-                      Number => 15 
-                    ),
-               array( Title => "daisy", 
-                      Price => 0.75,
-                      Number => 25,
-                    ),
-               array( Title => "orchid", 
-                      Price => 1.15,
-                      Number => 7 
-                    )
+$playerlist = array(array(array("rose", 1.25, 15),
+                    array("daisy", 0.75, 25),
+                    array("orchid", 1.15, 7) 
+                   ),
+              array(array("rose", 1.25, 15),
+                    array("daisy", 0.75, 25),
+                    array("orchid", 1.15, 7) 
+                   ),
+              array(array("rose", 1.25, 15),
+                    array("daisy", 0.75, 25),
+                    array("orchid", 1.15, 7) 
+                   )
              ); 
 
 // function call to convert array to xml
@@ -54,6 +55,8 @@ array_to_xml($playerlist,$xml_playerlist);
 //saving generated xml file
 $xml_playerlist->asXML('output.xml');
 
+//$xml = Array2XML::createXML('root_node_name', $playerlist);
+//echo $xml->saveXML();
 
 // function defination to convert array to xml
 function array_to_xml($playerlist, &$xml_playerlist) {

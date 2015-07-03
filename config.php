e083452edb318646f1c65047dc14813c942f1213
axi92 <phips_92@yahoo.de>
2015-07-03 08:56:25 +0200

    Changed ARK-IP

--- config.php
+++ config.php
@@ -4,6 +4,6 @@ define( 'SQ_SERVER_PORT', 2303 ); //arma 3
 define( 'SQ_TIMEOUT',     1 );
 define( 'SQ_ENGINE',      SourceQuery :: SOURCE );	
 
-define( 'SQ_SERVER_ADDR_ARK', '31.214.216.128' ); //zero one ark
+define( 'SQ_SERVER_ADDR_ARK', '94.23.217.142' ); //zero one ark
 define( 'SQ_SERVER_PORT_ARK', 27016 ); //ark
 ?>
